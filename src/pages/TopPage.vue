1d38ac3948d6149cf347a2c80e76651f3a07c8da
asukachikaru <asukachikaru@gmail.com>
2021-02-06 19:25:36 +0900

    feat: add transform animation to top

--- src/pages/TopPage.vue
+++ src/pages/TopPage.vue
@@ -27,48 +27,48 @@ export default TopPage;
 .top {
   padding: 50px 0;
   max-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  position: relative;
 }
 
 .top-title {
+  position: relative;
   font-weight: 900;
   font-size: 128px;
   font-family: "Coda";
   line-height: 137.5px;
   color: #00938e;
   text-align: right;
-  animation: fadein 0.9s linear;
+  animation: fadein 0.5s linear;
 }
 
 @keyframes fadein {
   0% {
-    opacity: 0.2;
+    opacity: 0.1;
+    transform: translateX(-50px);
   }
   20% {
-    opacity: 0.4;
+    opacity: 0.2;
   }
   30% {
-    opacity: 0.55;
-  }
-  40% {
-    opacity: 0.7;
-  }
-  50% {
-    opacity: 0.8;
+    opacity: 0.3;
   }
   60% {
-    opacity: 0.85;
+    transform: translateX(-15px);
   }
   70% {
-    opacity: 0.9;
+    transform: translateX(-10px);
   }
   80% {
-    opacity: 0.95;
+    opacity: 0.7;
+    transform: translateX(-5px);
+  }
+  90% {
+    opacity: 0.9;
+    transform: translateX(-2px);
   }
   100% {
     opacity: 1;
+    transform: translateX(0px);
   }
 }
 @media (min-height: 1000px) {
